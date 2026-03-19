5fa812e25f2878d6da8ad954cce68bb9a6b21f79
Андрей Левин <alevin@sdvor.com>
2026-03-19 14:59:19 +0500

    magic-coke

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+// Middleware .!.
 app.use(cors());
 app.use(express.json());
 
